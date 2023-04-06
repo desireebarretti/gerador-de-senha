6f8ba5bcace51b9c966ec309ad11b2adf19c6757
desireebarretti <110750885+desireebarretti@users.noreply.github.com>
2023-04-05 22:25:40 -0300

    Criando o javaScript

---
+++ javascript/js.js
@@ -0,0 +1,28 @@
+// relativo ao tamanho da senha
+let txtTamanhoSenha = document.querySelector('#valor');
+let inputRange = document.querySelector('#tamanho-senha');
+
+// bloco de senha e seus displays
+let btnGerarSenha = document.querySelector('#btn-gerar');
+let senhaGerada = document.querySelector('#senha-gerada');
+let copiarSenha = document.querySelector('#copiar-senha');
+
+// este é o algoritmo da geração de senha
+let charset = "abcdefghijlmnopqurstuvxzABCDEFGHIJKLMNOPQRSTUVXZ0123456789!@-&*"
+
+txtTamanhoSenha.innerHTML = inputRange.value
+
+function modificarRange() {
+    txtTamanhoSenha.innerHTML = inputRange.value;
+}
+
+function gerarSenha() {
+   senhaGerada.style.display = 'block';
+   copiarSenha.style.display = 'block';
+
+   let novaSenha = "";
+   for(i = 0, n = charset.length; i < inputRange.value; i++){   
+        novaSenha += charset.charAt(Math.random()*n)
+    }
+    console.log(senhaGerada.value = novaSenha)
+}
